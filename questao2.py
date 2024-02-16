169c2679d915f9d427578bb1a82bc0d13d5fd956
profgabrielferreira <134711762+profgabrielferreira@users.noreply.github.com>
2024-02-16 08:57:11 -0300

    Add files via upload

---
+++ questao2.py
@@ -0,0 +1,11 @@
+# 2) crie um programa que some 4 numeros e calcule a media,  
+# depois mostre o valor
+
+num1 = float(input("informe o primeiro valor: "))
+num2 = float(input("informe o segundo valor: "))
+num3 = float(input("informe o terceiro valor: "))
+num4 = float(input("informe o quarto valor: "))
+
+media = ((num1 + num2 + num3 + num4) /  4)
+
+print(" a media eh: ", media)
---
+++ questap1.py
@@ -0,0 +1,10 @@
+# 1) crie um programa que peça 3 notas e some as três, 
+# depois mostre o valor
+
+nota1 = float(input("informe a nota 1: ")) 
+nota2 = float(input("informe a nota 2: ")) 
+nota3 = float(input("informe a nota 3: ")) 
+
+somaNotas = nota1 + nota2 + nota3 
+
+print('a soma das notas eh: ', somaNotas)
